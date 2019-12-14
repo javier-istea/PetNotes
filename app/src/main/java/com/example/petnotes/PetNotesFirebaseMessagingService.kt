08d8134f10233cf3712bc01cc0a67ab53f19c23e
JavierSpano <javi.spano@hotmail.com>
2019-12-14 12:20:05 -0300

    -

--- app/src/main/java/com/example/petnotes/PetNotesFirebaseMessagingService.kt
+++ app/src/main/java/com/example/petnotes/PetNotesFirebaseMessagingService.kt
@@ -1,11 +1,56 @@
 package com.example.petnotes
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 
 class PetNotesFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val PUSH_NOTIF_CHANNEL_ID: String = "0"
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        NotificationHandler.showNotification()
+        super.onMessageReceived(remoteMessage)
+        val body = remoteMessage.notification?.body
+        sendNotification(body)
+    }
+
+    private fun sendNotification(body: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val notificationBuilder = NotificationCompat.Builder(this, PUSH_NOTIF_CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createPushNotifChannel(notificationManager)
+        notificationManager.notify(0, notificationBuilder.build())
+
+    }
+
+    private fun createPushNotifChannel(notificationManager: NotificationManager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                PUSH_NOTIF_CHANNEL_ID,
+                "Push Notification",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
